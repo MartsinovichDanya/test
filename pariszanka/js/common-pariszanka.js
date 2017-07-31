9bcc144860484db9b237c9954db48f5db6b56aad
MartsinovichDanya <martsinovich@gmail.com>
2017-07-31 18:05:33 +0300

    pariszanka

---
+++ pariszanka/js/common-pariszanka.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+
+
+var clock;
+clock = $('.clock').FlipClock({
+    clockFace: 'DailyCounter', //вид счетчика (с количеством дней)
+    autoStart: false, //Отключаем автозапуск
+    countdown: true	, //Отсчет назад
+    showSeconds:false,
+    language:'ru-ru', //Локаль языка
+    callbacks: { //Действие после окончания отсчета
+    stop: function() {
+		$('.clock-stop').addClass("Акция окончена");
+		$('.clock-stop').html('Время вышло!');
+       	}
+       }
+    });
+		var target = "2017-07-28T11:11:11"; //Дата до которой нужен таймер
+		var targetDate = new Date(target); //Переводим в мил. секунды
+		var nowDate = Date.now(); //Текущая дата и время в мил. секундах
+		var time = (targetDate - nowDate) / 1000; //Получаем секунды
+  
+    clock.setTime(time); //Устанавливаем нужное время в секундах
+    clock.setCountdown(true); //Устанавливаем отсчет назад
+    clock.start(); //Запускаем отсчет
+});
+	
+	//Таймер обратного отсчета
+	//Документация: http://keith-wood.name/countdown.html
+	//<div class="countdown" date-time="2015-01-07"></div>
+	var austDay = new Date($(".countdown").attr("date-time"));
+	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	$(".top_mnu").navigation();
+
+	//Добавляет классы дочерним блокам .block для анимации
+	//Документация: http://imakewebthings.com/jquery-waypoints/
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//Плавный скролл до блока .div по клику на .scroll
+	//Документация: https://github.com/flesler/jquery.scrollTo
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	var owl = $(".carousel");
+	owl.owlCarousel({
+		items : 3,
+		autoHeight : true
+	});
+	owl.on("mousewheel", ".owl-wrapper", function (e) {
+		if (e.deltaY > 0) {
+			owl.trigger("owl.prev");
+		} else {
+			owl.trigger("owl.next");
+		}
+		e.preventDefault();
+	});
+	$(".next_button").click(function() {
+		owl.trigger("owl.next");
+	});
+	$(".prev_button").click(function() {
+		owl.trigger("owl.prev");
+	});
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#callback").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("#callback").serialize()
+		}).done(function() {
+			alert("Спасибо. Ожидайте");
+			setTimeout(function() {
+				
+		});
+		return false;
+	});
+
+});
